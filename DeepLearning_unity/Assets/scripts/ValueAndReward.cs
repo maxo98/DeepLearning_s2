1def4c40a0bf348f29372e156a075a1c797fab44
erwan-ash <60441163+erwan-ash@users.noreply.github.com>
2023-02-15 16:40:47 +0100

    Grille + creation des scripts

---
+++ DeepLearning_unity/Assets/scripts/ValueAndReward.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueAndReward : MonoBehaviour
+{
+    public float stateValue;
+    public float[] stateReward; // 0: Up  |  1: Right  |  2: Down  |  3: Left
+}
